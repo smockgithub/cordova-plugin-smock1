77d442d94be3712c27c0cb967f688e9ae87b20d4
Unify.dy <smock130_163@163.com>
2018-06-19 00:49:37 +0800

    1

--- src/android/CordovaPluginSmock1.java
+++ src/android/CordovaPluginSmock1.java
@@ -15,6 +15,24 @@ import android.widget.Toast;
 public class CordovaPluginSmock1 extends CordovaPlugin {
 
     @Override
+    protected void pluginInitialize() {
+
+        super.pluginInitialize();
+
+        //String id = getAppId();
+
+        // save app id
+        //saveAppId(cordova.getActivity(), id);
+
+        // init api
+        //initWXAPI();
+
+        
+
+        //Log.d(TAG, "plugin initialized.");
+    }
+
+    @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("coolMethod")) {
             String message = args.getString(0);
@@ -30,6 +48,16 @@ public class CordovaPluginSmock1 extends CordovaPlugin {
         } else {
             Toast.makeText(cordova.getActivity(), "传入参数错误", Toast.LENGTH_LONG).show();
             callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+
+    private void startJD(String message, CallbackContext callbackContext){
+        if (message != null && message.length() > 0) {
+            callbackContext.success(message);
+            //Toast.makeText(cordova.getActivity(), cordova.getActivity(), Toast.LENGTH_LONG).show();
+        } else {
+            //Toast.makeText(cordova.getActivity(), "传入参数错误", Toast.LENGTH_LONG).show();
+            callbackContext.error("传入参数错误");
         }
     }
 }
--- www/CordovaPluginSmock1.js
+++ www/CordovaPluginSmock1.js
@@ -1,5 +1,13 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'CordovaPluginSmock1', 'coolMethod', [arg0]);
-};
+
+module.exports = {
+    coolMethod:function (arg0, success, error) {
+        exec(success, error, 'CordovaPluginSmock1', 'coolMethod', [arg0]);
+    },
+
+    startJD:function(arg0, success, error){
+        exec(success, error, 'CordovaPluginSmock1', 'startJD', [arg0]);
+    }
+
+}
